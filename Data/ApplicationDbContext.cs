f7e81d458bd51487d94e6830b44647d9220b2389
bill7maxmaye <bilenmehalek7@gmail.com>
2024-04-09 15:52:58 +0300

    Add the entity creation functionality

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,9 +9,19 @@ namespace BulkyWeb.Data
         {
             
         }
-
+        //the dbset is used to create tables in the db
         public DbSet<Category> Categories{ get; set;}
 
+        //the OnModelCreating is used to insert records to the table in the db
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Category>().HasData(
+                new Category {Id= 1, Name = "IPHOME", DisplayOrder =1},
+                new Category {Id =2, Name = "IPPHONE", DisplayOrder = 2 },
+                new Category {Id= 3 , Name = "laptop" ,  DisplayOrder=3}
+
+            );
+        }
     }
 
  }
